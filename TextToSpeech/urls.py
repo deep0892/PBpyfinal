e4e03a874a05ff5406baeb1e8f25c899a7c48e90
pratiksinghchauhan <pratiksingh773@gmail.com>
2017-08-29 02:30:27 +0530

    delete irrelevant

--- TextToSpeech/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from TextToSpeech.views import pollyexotel,samedayexpiryIVR,getfinaldetails,hardcopyrecievalIVR
-
-urlpatterns = [
-    url(r'polly', pollyexotel),    
-    url(r'samedayexpiryIVR', samedayexpiryIVR),
-    url(r'finalcalldetails', getfinaldetails), 
-    url(r'hardcopyrecievalIVR', hardcopyrecievalIVR), 
-]
